88d7176027eb359c1d013467ef54ca82770ad477
Shadow <william.shadow@outlook.com>
2022-07-11 06:54:18 +0000

    Change to spudcraft name

--- config.js
+++ config.js
@@ -11,10 +11,10 @@ var ip_copied = "IP copied!";
 var firefly_count = 20;
 
 staff("Ibotmealot", "a768e7e5-59af-43b2-b871-8374947a27e4", "Owner");
+staff("TheShadow", "b4cf2224-e6dd-427f-882d-89d59a8d3447", "Administrator");
 staff("DarkLotus98", "aba060b6-4299-47da-9d30-d8c89d9255d1", "Staff");
 staff("Joesh_", "01dfe815-4b67-4fd0-a7a1-a14a64a1625c", "Staff");
 staff("safirras", "d6adab5f-bf21-453c-bae1-9c2cace7913d", "Staff");
-staff("TheShadow", "b4cf2224-e6dd-427f-882d-89d59a8d3447", "Staff");
 staff("A_Trapp", "8dc9cefd-7c5f-4321-bd4f-39cd440dacb7", "Staff");
 
 vote("Planet-Minecraft", "https://i.postimg.cc/9fmyrRkJ/Transparent-Planet-Minecraft-Logo.png", "https://planetminecraft.com");

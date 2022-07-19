64051a4a0f893b7cb93fcf3525b2bd361697b22d
Ibotmealot <42391199+Ibotmealot@users.noreply.github.com>
2022-07-19 16:32:07 -0400

    Add MintPaint and remove DarkLotus

--- config.js
+++ config.js
@@ -12,10 +12,10 @@ var firefly_count = 20;
 
 staff("Ibotmealot", "a768e7e5-59af-43b2-b871-8374947a27e4", "Owner");
 staff("TheShadow", "b4cf2224-e6dd-427f-882d-89d59a8d3447", "Administrator");
-staff("DarkLotus98", "aba060b6-4299-47da-9d30-d8c89d9255d1", "Staff");
 staff("Joesh_", "01dfe815-4b67-4fd0-a7a1-a14a64a1625c", "Staff");
 staff("A_Trapp", "8dc9cefd-7c5f-4321-bd4f-39cd440dacb7", "Staff");
 staff("Labyrinthist", "8e96503f-65f8-4bf3-b9b8-90e32c80c08e", "Staff");
+staff("MintPaint", "a7408905-0da0-4765-ad92-b8de7e2cd1bb", "Staff");
 
 vote("Planet-Minecraft", "https://i.postimg.cc/9fmyrRkJ/Transparent-Planet-Minecraft-Logo.png", "https://planetminecraft.com");
 vote("Minecraft Servers Org", "https://static.wikia.nocookie.net/minecraft_gamepedia/images/2/2d/Plains_Grass_Block.png", "https://minecraftservers.org/");

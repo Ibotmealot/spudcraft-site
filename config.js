f0b64f1e940532a89b402dd150b2512a693db5fe
Ibotmealot <42391199+Ibotmealot@users.noreply.github.com>
2022-09-04 14:21:06 -0400

    joesh resigned

--- config.js
+++ config.js
@@ -11,7 +11,6 @@ var firefly_count = 20;
 
 staff("Ibotmealot", "a768e7e5-59af-43b2-b871-8374947a27e4", "Owner");
 staff("TheShadow", "b4cf2224-e6dd-427f-882d-89d59a8d3447", "Administrator");
-staff("Joesh_", "01dfe815-4b67-4fd0-a7a1-a14a64a1625c", "Staff");
 staff("A_Trapp", "8dc9cefd-7c5f-4321-bd4f-39cd440dacb7", "Staff");
 staff("Labyrinthist", "8e96503f-65f8-4bf3-b9b8-90e32c80c08e", "Staff");
 staff("MintPaint", "a7408905-0da0-4765-ad92-b8de7e2cd1bb", "Staff");

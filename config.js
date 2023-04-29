f54ea43aeead7cc7ce7280b6496a9f2a390e952a
Ibotmealot <42391199+Ibotmealot@users.noreply.github.com>
2023-04-29 15:24:21 -0400

    Update the staff members

--- config.js
+++ config.js
@@ -10,10 +10,12 @@ var ip_copied = "IP copied!";
 var firefly_count = 20;
 
 staff("Ibotmealot", "a768e7e5-59af-43b2-b871-8374947a27e4", "Owner");
-staff("TheShadow", "b4cf2224-e6dd-427f-882d-89d59a8d3447", "Administrator");
-staff("A_Trapp", "8dc9cefd-7c5f-4321-bd4f-39cd440dacb7", "Staff");
-staff("Labyrinthist", "8e96503f-65f8-4bf3-b9b8-90e32c80c08e", "Staff");
-staff("MintPaint", "a7408905-0da0-4765-ad92-b8de7e2cd1bb", "Staff");
+staff("A_Trapp", "8dc9cefd-7c5f-4321-bd4f-39cd440dacb7", "Administrator");
+staff("Labyrinthist", "8e96503f-65f8-4bf3-b9b8-90e32c80c08e", "Administrator");
+staff("MintPaint", "a7408905-0da0-4765-ad92-b8de7e2cd1bb", "Administrator");
+staff("softshadows", "b4cf2224-e6dd-427f-882d-89d59a8d3447", "Administrator");
+staff("Akuste", "90b0d231-751d-49bd-96d1-af0b43eacd3d", "Staff");
+staff("PeskyBird58", "093b12b6-4093-4ffd-aeed-9a2390d7d3d5", "Staff");
 
 vote("Planet-Minecraft", "https://i.postimg.cc/9fmyrRkJ/Transparent-Planet-Minecraft-Logo.png", "https://planetminecraft.com");
 vote("Minecraft Servers Org", "https://static.wikia.nocookie.net/minecraft_gamepedia/images/2/2d/Plains_Grass_Block.png", "https://minecraftservers.org/");

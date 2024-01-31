90efb5da2d76f8db9937fbae23a5bf49bbfa3ff9
Shadow <shadow@buape.com>
2024-01-31 14:21:01 -0600

    Update config.js

--- config.js
+++ config.js
@@ -15,7 +15,8 @@ staff("Labyrinthist", "8e96503f-65f8-4bf3-b9b8-90e32c80c08e", "Administrator");
 staff("softshadows", "b4cf2224-e6dd-427f-882d-89d59a8d3447", "Administrator");
 staff("OccasionallyAsh", "07eade2a-b5c8-4dad-b3cb-b5821bbdf905", "Staff");
 staff(".Karasu0w0", "00000000-0000-0000-0009-01f800442661", "Staff");
-staff("PeskyBird58", "093b12b6-4093-4ffd-aeed-9a2390d7d3d5", "Staff");
+staff("Peskatium", "093b12b6-4093-4ffd-aeed-9a2390d7d3d5", "Staff");
+staff("Levniv", "06afa24e-71cc-4858-9daa-1874f7309e45", "Staff");
 
 vote("Planet-Minecraft", "https://i.postimg.cc/9fmyrRkJ/Transparent-Planet-Minecraft-Logo.png", "https://planetminecraft.com");
 vote("Minecraft Servers Org", "https://static.wikia.nocookie.net/minecraft_gamepedia/images/2/2d/Plains_Grass_Block.png", "https://minecraftservers.org/");
